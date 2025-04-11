0dffbd4a67fa435bb882b339ac266ce00c610511
Charbrown08 <carlos.moreno.jimenez@konecta-group.com>
2025-04-11 14:33:37 -0500

    [ADD] define AWS provider and resources for observability setup

---
+++ terraform/main.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "observability_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Allow SSH, Prometheus and Grafana ports"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ips
+  }
+
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ips
+  }
+
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ips
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
+
+resource "aws_instance" "observability_ec2" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [aws_security_group.observability_sg.id]
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+  user_data                   = file("${path.module}/../scripts/userdata.sh")
+
+  tags = {
+    Name        = "${var.project_name}-ec2"
+    environment = var.environment
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,55 @@
+# Región donde se desplegará la infraestructura (ej. us-east-1)
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+}
+
+# AMI ID para lanzar la instancia (Amazon Linux 2 recomendado)
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+# Tipo de instancia EC2
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+  default     = "t3.micro"
+}
+
+# ID del par de claves SSH creado en AWS
+variable "key_name" {
+  description = "Name of the SSH key pair to access the instance"
+  type        = string
+}
+
+# ID de la VPC donde se lanzará la EC2
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+# ID de la subred donde se ubicará la instancia
+variable "subnet_id" {
+  description = "ID of the subnet to launch the EC2 instance in"
+  type        = string
+}
+
+# Lista de IPs permitidas para conectarse (ej. ["0.0.0.0/0"])
+variable "allowed_ips" {
+  description = "List of CIDR blocks allowed to access the instance"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+# Nombre del proyecto o prefijo para los recursos
+variable "project_name" {
+  description = "Name prefix for all resources"
+  type        = string
+}
+
+# Entorno (staging, production, etc)
+variable "environment" {
+  description = "Environment name for tagging purposes"
+  type        = string
+}
